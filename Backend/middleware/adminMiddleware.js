02306acf91c4157637ecc3105d46035c735a3096
Saurabh Yadav <152073566+voyager2003@users.noreply.github.com>
2026-05-17 14:13:23 +0530

    Delete Backend directory

--- Backend/middleware/adminMiddleware.js
+++
@@ -1,9 +0,0 @@
-const adminOnly = (req, res, next) => {
-  if (req.user && req.user.role === 'admin') {
-    next();
-  } else {
-    res.status(403).json({ message: 'Access denied: Admins only' });
-  }
-};
-
-module.exports = { adminOnly };
